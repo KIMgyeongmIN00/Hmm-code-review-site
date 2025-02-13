b4ba50475c4d9c5764979a23e8912bb054d270a5
choichangyeon <changyon99@naver.com>
2025-02-13 21:04:31 +0900

    design:#4 헤더 요소 배치 수정

--- src/components/layouts/header/Header.jsx
+++ src/components/layouts/header/Header.jsx
@@ -9,30 +9,39 @@ export default function Header() {
   return (
     <StContainer>
       <LogoImg src="/image/logo.png" />
-      <SearchInput />
-      {IsAuth ? <MyPageBtn /> : <AuthBtn />}
+      <StWrapper>
+        <SearchInput />
+        {IsAuth ? <MyPageBtn /> : <AuthBtn />}
+      </StWrapper>
     </StContainer>
   );
 }
 
 const LogoImg = styled.img`
+  margin: 0 0 0 40px;
   width: 60px;
 `;
 
 const StContainer = styled.div`
   display: flex;
-  flex-direction: row-reverse;
+  flex-direction: row;
   align-items: center;
+  justify-content: space-between;
   background: #ffffff;
   width: 100%;
   height: 80px;
   border-bottom: 1px solid #b2b9c0;
-
-  gap: 20px;
 
   font-family: 'Open Sans';
   font-size: 16px;
   line-height: 24px;
 
   color: #666666;
+`;
+
+const StWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 20px;
 `;
