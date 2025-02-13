daed52c5a8f54e8351ae35eb1479df057fee608d
llddang <bodyness@naver.com>
2025-02-13 16:49:45 +0900

    fix:#9 fix global css syntax
    #9

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -1,9 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  --color-main: #495057;
-  --color-point: #dec0ff;
-  --color-border: #B2B9C0;
+  :root{
+    --color-main: #495057;
+    --color-point: #dec0ff;
+    --color-border: #b076f0;
+  }
 `;
 
 export default GlobalStyle;
