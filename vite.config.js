fc3b326da00a82c01ad22c381fe8368a4626fcf5
llddang <bodyness@naver.com>
2025-02-13 15:29:04 +0900

    chore: vite의  절대 경로 설정
    #1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+      { find: '@common', replacement: '/src/components/common' },
+      { find: '@features', replacement: '/src/components/features' },
+      { find: '@layer', replacement: '/src/components/layer' },
+      { find: '@constants', replacement: '/src/constants' },
+      { find: '@pages', replacement: '/src/pages' }
+    ]
+  }
+});
