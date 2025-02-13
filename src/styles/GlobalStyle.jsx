7f0c581a62dc3188a2fecbd837d580a99f01aa0d
llddang <bodyness@naver.com>
2025-02-13 15:34:46 +0900

    feat: global style 설정

---
+++ src/styles/GlobalStyle.jsx
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  --color-main: #495057;
+  --color-point: #dec0ff;
+  --color-border: #B2B9C0;
+`;
+
+export default GlobalStyle;
