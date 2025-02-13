6f24881c740a9101a138cc74c7c492b63601f930
llddang <bodyness@naver.com>
2025-02-13 16:50:23 +0900

    feat:#9 add round style to global css

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -9,6 +9,12 @@ const GlobalStyle = createGlobalStyle`
     --color-point-light: #f5eaff;
     --color-point-dark: #b699d6;
     --color-border: #b076f0;
+
+    --round-sm: 4px;
+    --round-md: 6px;
+    --round-lg: 12px;
+    --round-xl: 20px;
+  }
   }
 `;
 
