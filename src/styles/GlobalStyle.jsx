71e3571bc05f1fbb03c6e70063f9484442dc7f50
llddang <bodyness@naver.com>
2025-02-13 16:50:04 +0900

    feat:#9 add color type to global css

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -3,7 +3,11 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   :root{
     --color-main: #495057;
+    --color-main-light: #9fa3a7;
+    --color-main-dark: #272a2d;
     --color-point: #dec0ff;
+    --color-point-light: #f5eaff;
+    --color-point-dark: #b699d6;
     --color-border: #b076f0;
   }
 `;
