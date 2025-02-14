ccf8d5ea7f54d26405e50f08fb4297a4af060fa4
choichangyeon <changyon99@naver.com>
2025-02-14 23:18:37 +0900

    refactor:#14 컴포넌트 삭제 import 변경

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,11 +1,15 @@
 import { styled } from 'styled-components';
-import HomeFilterTab from '@features/home/HomeFilterTab';
+import HomeLanguageSelector from '@features/home/HomeLanguageSelector';
+import HomePostSortRadioGroup from '@features/home/HomePostSortRadioGroup';
 import HomePost from '@features/home/HomePost';
 
 export default function HomePage() {
   return (
     <StHomePageContainer>
-      <HomeFilterTab />
+      <StFilterPanelContainer>
+        <HomeLanguageSelector />
+        <HomePostSortRadioGroup />
+      </StFilterPanelContainer>
       <HomePost />
     </StHomePageContainer>
   );
@@ -14,4 +18,14 @@ const StHomePageContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
+`;
+
+const StFilterPanelContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: flex-end;
+  width: 100%;
+  height: 60px;
+  gap: 20px;
 `;
