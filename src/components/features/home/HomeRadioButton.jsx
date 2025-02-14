c5f948b22ed7bdea19baa09b36b6f78193d8d339
choichangyeon <changyon99@naver.com>
2025-02-14 23:00:48 +0900

    refactor:#14 라디오 버튼 스타일 코드 수정

--- src/components/features/home/HomeRadioButton.jsx
+++ src/components/features/home/HomeRadioButton.jsx
@@ -7,33 +7,33 @@ export default function HomeRadioButton() {
 
   return (
     <StRadioContainer>
-      <StRadioButton
+      <input
         id={sortConstants.SORT_LATEST}
         name="sort"
         type="radio"
         value={sortConstants.SORT_LATEST}
         checked={sort === sortConstants.SORT_LATEST}
         onChange={(e) => setSort(e.target.value)}
-      ></StRadioButton>
-      <StRadioLabel htmlFor={sortConstants.SORT_LATEST}>최신순</StRadioLabel>
-      <StRadioButton
+      ></input>
+      <label htmlFor={sortConstants.SORT_LATEST}>최신순</label>
+      <input
         id={sortConstants.SORT_LIKE}
         name="sort"
         type="radio"
         value={sortConstants.SORT_LIKE}
         checked={sort === sortConstants.SORT_LIKE}
         onChange={(e) => setSort(e.target.value)}
-      ></StRadioButton>
-      <StRadioLabel htmlFor={sortConstants.SORT_LIKE}>좋아요순</StRadioLabel>
-      <StRadioButton
+      ></input>
+      <label htmlFor={sortConstants.SORT_LIKE}>좋아요순</label>
+      <input
         id={sortConstants.SORT_COMMENT}
         name="sort"
         type="radio"
         value={sortConstants.SORT_COMMENT}
         checked={sort === sortConstants.SORT_COMMENT}
         onChange={(e) => setSort(e.target.value)}
-      ></StRadioButton>
-      <StRadioLabel htmlFor={sortConstants.SORT_COMMENT}>댓글순</StRadioLabel>
+      ></input>
+      <label htmlFor={sortConstants.SORT_COMMENT}>댓글순</label>
     </StRadioContainer>
   );
 }
@@ -52,32 +52,32 @@ const StRadioContainer = styled.div`
   border-radius: var(--round-sm);
   margin: 0 20px 0 0;
   overflow: hidden;
-`;
 
-const StRadioButton = styled.input`
-  height: var(--height-md);
-  display: none;
-`;
-
-const StRadioLabel = styled.label`
-  display: flex;
-  width: 45px;
-  align-items: center;
-  justify-content: center;
-  font-size: var(--font-size-sm);
-
-  padding: 10px 10px;
-  cursor: pointer;
-
-  &:not(:last-of-type) {
-    border-right: 1px solid var(--color-border);
+  & > input {
+    height: var(--height-md);
+    display: none;
   }
 
-  &:hover {
-    background-color: var(--color-hover);
+  & > label {
+    display: flex;
+    width: 45px;
+    align-items: center;
+    justify-content: center;
+    font-size: var(--font-size-sm);
+
+    padding: 10px 10px;
+    cursor: pointer;
+
+    &:not(:last-of-type) {
+      border-right: 1px solid var(--color-border);
+    }
+
+    &:hover {
+      background-color: var(--color-hover);
+    }
   }
 
-  ${StRadioButton}:checked + & {
+  & > input[type='radio']:checked + label {
     background-color: var(--color-point);
     color: var(--color-main);
   }
