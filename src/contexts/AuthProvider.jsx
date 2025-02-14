e85c60e30c2bb38b4768034035ef83632e373e3c
SuimKim <github suim0215@gmail.com>
2025-02-14 15:04:11 +0900

    feat:#22 AuthProvider 생성

---
+++ src/contexts/AuthProvider.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useReducer } from 'react';
+import { AuthContext } from './auth.context';
+import authReducer from './auth.reducer';
+import supabase from '../lib/supabase/supabase.api';
+
+export default function AuthProvider({ children }) {
+  const [isLogin, dispatch] = useReducer(authReducer, false);
+
+  useEffect(() => {
+    const {
+      data: { subscription }
+    } = supabase.auth.onAuthStateChange((event, session) => {
+      if (session) {
+        dispatch({ payload: true });
+      } else {
+        dispatch({ payload: false });
+      }
+    });
+    return () => subscription.unsubscribe();
+  }, []);
+
+  return <AuthContext.Provider value={{ isLogin }}>{children}</AuthContext.Provider>;
+}
