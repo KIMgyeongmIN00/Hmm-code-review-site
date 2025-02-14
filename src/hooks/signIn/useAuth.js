19e59869451102bf06a8551971dfc99a9fb3b929
LSJ0706 <zosksldhs@gmail.com>
2025-02-14 19:44:37 +0900

    feat:#19 useAuth customHook 추가

---
+++ src/hooks/signIn/useAuth.js
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import supabase from '@libs/supabase/supabase';
+
+export default function useAuth() {
+  const [email, SetEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const navigate = useNavigate();
+
+  function handleEmailChange(e) {
+    SetEmail(e.target.value);
+  }
+
+  function handlePasswordChange(e) {
+    setPassword(e.target.value);
+  }
+
+  async function handleSignIn(e) {
+    e.preventDefault();
+    try {
+      const { error } = await supabase.auth.signInWithPassword({ email, password });
+      if (error) throw error;
+      alert('로그인 성공!');
+      navigate('/');
+    } catch (error) {
+      alert('아이이디와 비밀번호가 일치하지 않습니다!');
+      console.error(error);
+    }
+  }
+
+  return {
+    email,
+    password,
+    handleEmailChange,
+    handlePasswordChange,
+    handleSignIn
+  };
+}
