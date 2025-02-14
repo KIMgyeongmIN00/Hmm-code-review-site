ebf98dd443c6e7dfdeeaf4417719d73846089696
choichangyeon <changyon99@naver.com>
2025-02-14 10:25:37 +0900

    design:#4 ButtonLink 적용

--- src/components/layouts/header/AuthBtn.jsx
+++ src/components/layouts/header/AuthBtn.jsx
@@ -1,10 +1,24 @@
-import Button from '@commons/Button';
 import styled from 'styled-components';
+import Button from '@commons/Button';
+import ButtonLink from '@commons/ButtonLink';
 
 export default function AuthBtn() {
-  return <StContainer $variant="ghost">로그인 / 회원가입</StContainer>;
+  return (
+    <StContainer>
+      <ButtonLink $variant="ghost" $size="sm">
+        로그인
+      </ButtonLink>
+      /
+      <ButtonLink $variant="ghost" $size="sm">
+        회원가입
+      </ButtonLink>
+    </StContainer>
+  );
 }
 
-const StContainer = styled(Button)`
+const StContainer = styled.div`
   height: 40px;
+  display: flex;
+  flex-wrap: row;
+  align-items: center;
 `;
