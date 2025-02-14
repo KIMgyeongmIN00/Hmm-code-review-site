5af29e2e4a8bb26cffadd65e315dbcfaf4bd24b1
choichangyeon <changyon99@naver.com>
2025-02-14 15:39:17 +0900

    feat:#14 필터링 컴포넌트 생성

---
+++ src/pages/home/HomeFilter.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function HomeFilter() {
+  return <div>HomeFilter</div>;
+}
--- src/pages/home/MainPage.jsx
+++ src/pages/home/MainPage.jsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { styled } from 'styled-components';
+import HomeFilter from '@pages/home/HomeFilter';
 
 export default function MainPage() {
-  return <div>MainPage</div>;
+  return (
+    <StMainContainer>
+      <HomeFilter />
+    </StMainContainer>
+  );
 }
+
+const StMainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
