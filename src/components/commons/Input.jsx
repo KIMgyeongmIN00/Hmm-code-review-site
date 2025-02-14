cb0d97e2f5e0761276c95fcd6fadac81bd23a1a0
LSJ0706 <zosksldhs@gmail.com>
2025-02-14 19:46:40 +0900

    feat:#19 Input Component width속성 추가, global CSS 적용

--- src/components/commons/Input.jsx
+++ src/components/commons/Input.jsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
-export default function Input({ icon: Icon, ...props }) {
+export default function Input({ icon: Icon, width, ...props }) {
   return (
-    <StInputContainer>
+    <StInputContainer width={width}>
       {/*Icon이 있을 때만 이미지 표현*/}
       {Icon && <Icon />}
       <StInput {...props} />
@@ -16,17 +16,17 @@ const StInputContainer = styled.div`
   border: 2px solid var(--color-border);
   border-radius: var(--round-md);
   padding: 4px 8px;
-  width: 320px;
+  width: ${(props) => props.width || '100%'};
   height: var(--height-md);
 
   & > svg {
-    font-size: 20px;
+    font-size: var(--font-size-lg);
     margin-right: 4px;
   }
 `;
 const StInput = styled.input`
   border: none;
   outline: none;
-  font-size: ${(props) => props.fontSize || '16px'};
+  font-size: ${(props) => props.fontSize || 'var(--font-size-md)'};
   flex: 1;
 `;
