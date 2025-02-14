23980b512702788c9d6115e45d388cd60f918c86
choichangyeon <changyon99@naver.com>
2025-02-14 14:42:25 +0900

    design:#4 검색창 디자인 수정

--- src/components/layouts/header/SearchInput.jsx
+++ src/components/layouts/header/SearchInput.jsx
@@ -6,11 +6,4 @@ export default function SearchInput() {
   return <StInput icon={MdOutlineSearch} placeholder="search keyword..."></StInput>;
 }
 
-const SearchIcon = styled(MdOutlineSearch)`
-  margin: 0 5px 0 5px;
-  color: var(--color-main-light);
-`;
-
-const StInput = styled(Input)`
-  border-radius: var(--round-full);
-`;
+const StInput = styled(Input)``;
