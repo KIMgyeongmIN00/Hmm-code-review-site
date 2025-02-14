d3a828ac1b14257a39f5eaaf458a2c60349ad75c
choichangyeon <changyon99@naver.com>
2025-02-14 16:22:29 +0900

    design:#14 필터링 컴포넌트 디자인

--- src/components/features/home/HomeFilter.jsx
+++ src/components/features/home/HomeFilter.jsx
@@ -1,3 +1,20 @@
+import styled from 'styled-components';
+
 export default function HomeFilter() {
-  return <div>HomeFilter</div>;
+  return (
+    <StFilterContainer>
+      <p>HomeFilter1</p>
+      <p>HomeFilter2</p>
+      <p>HomeFilter3</p>
+    </StFilterContainer>
+  );
 }
+
+const StFilterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: flex-end;
+  width: 100%;
+  height: 60px;
+`;
