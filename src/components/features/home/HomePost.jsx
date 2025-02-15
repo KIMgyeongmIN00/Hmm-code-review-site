ac94182fa0bf17657c8153e2d02139704ab928f5
choichangyeon <changyon99@naver.com>
2025-02-15 18:59:06 +0900

    feat:#14 포스터 요소 추가

--- src/components/features/home/HomePost.jsx
+++ src/components/features/home/HomePost.jsx
@@ -1,9 +1,20 @@
+import IconButton from '@commons/IconButton';
 import styled from 'styled-components';
+import { MdFavoriteBorder, MdFavorite, MdChatBubbleOutline } from 'react-icons/md';
+import { useState } from 'react';
 
 export default function HomePost() {
+  const [isActive, setIsActive] = useState(false);
   return (
     <StHomePostContainer>
-      <StPostTitle></StPostTitle>
+      <StPostTitle>Card Title</StPostTitle>
+      <StPostContent>this is test for develop.</StPostContent>
+      <IconButton activeIcon={MdFavorite} inActiveIcon={MdFavoriteBorder} onClick={(e) => setIsActive(!e)} />
+      <IconButton
+        activeIcon={MdChatBubbleOutline}
+        inActiveIcon={MdChatBubbleOutline}
+        onClick={console.log('gotoPage')}
+      />
     </StHomePostContainer>
   );
 }
@@ -13,6 +24,9 @@ const StHomePostContainer = styled.div`
   border-radius: var(--round-md);
   height: var(--height-xl);
   width: 800px;
+  padding: 10px;
 `;
 
-const StPostTitle = styled.h2``;
+const StPostTitle = styled.h1``;
+
+const StPostContent = styled.p``;
