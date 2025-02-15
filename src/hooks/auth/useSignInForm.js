a794bda9c6c609a316dae3682662e072d55659bb
LSJ0706 <zosksldhs@gmail.com>
2025-02-15 21:09:03 +0900

    feat:#19 signin error message 생성

--- src/hooks/auth/useSignInForm.js
+++ src/hooks/auth/useSignInForm.js
@@ -4,11 +4,11 @@ import supabase from '@libs/supabase';
 
 export default function useSignInForm() {
   const [signInForm, setSignInForm] = useState({ email: '', password: '' });
-
+  const [errorMessage, setErrorMessage] = useState('');
   const navigate = useNavigate();
+
   function handleSignInFormChange(e) {
     const { name, value } = e.target;
-    console.log(name);
     switch (name) {
       case 'email':
         return setSignInForm((prev) => ({ ...prev, email: value }));
@@ -17,23 +17,25 @@ export default function useSignInForm() {
       default:
         break;
     }
+    setErrorMessage('');
   }
 
   async function handleSignIn(e) {
     e.preventDefault();
     const { email, password } = signInForm;
     const { error } = await supabase.auth.signInWithPassword({ email, password });
+    console.log(error);
     if (error) {
-      alert('아이디와 비밀번호가 일치하지 않습니다!');
+      setErrorMessage('아이디와 비밀번호가 일치하지 않습니다!');
       throw error;
     } else {
-      alert('로그인 성공!');
       navigate('/');
     }
   }
 
   return {
     signInForm,
+    errorMessage,
     handleSignInFormChange,
     handleSignIn
   };
