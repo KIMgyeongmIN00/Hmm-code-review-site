8d6d5db9070cddcf3b6656e0f58f4b24a1e4d111
LSJ0706 <zosksldhs@gmail.com>
2025-02-15 22:52:10 +0900

    chore:#19 supabase 폴더 경로 변경

--- src/hooks/auth/useSignInForm.js
+++ src/hooks/auth/useSignInForm.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import supabase from '@libs/supabase';
+import supabase from '@/libs/api/supabase.api';
 
 export default function useSignInForm() {
   const [signInState, setSignInState] = useState({ email: '', password: '' });
