3656be733888956e3c1726e2961751737725627e
choichangyeon <changyon99@naver.com>
2025-02-15 18:18:28 +0900

    design:#14 홈 포스트 디자인

--- src/components/features/home/HomePost.jsx
+++ src/components/features/home/HomePost.jsx
@@ -1,3 +1,18 @@
+import styled from 'styled-components';
+
 export default function HomePost() {
-  return <div>HomePost</div>;
+  return (
+    <StHomePostContainer>
+      <StPostTitle></StPostTitle>
+    </StHomePostContainer>
+  );
 }
+
+const StHomePostContainer = styled.div`
+  border: 1px solid var(--color-border);
+  border-radius: var(--round-md);
+  height: 100px;
+  width: 800px;
+`;
+
+const StPostTitle = styled.h2``;
