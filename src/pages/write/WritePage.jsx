d64665b714fa1b43e88cc3f8751714298bdd4f62
llddang <bodyness@naver.com>
2025-02-16 20:37:08 +0900

    feat:#41 글 작성 페이지 supabase와 연결

--- src/pages/write/WritePage.jsx
+++ src/pages/write/WritePage.jsx
@@ -1,19 +1,34 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import MDEditor from '@uiw/react-md-editor';
 import Input from '@commons/Input';
 import Button from '@commons/Button';
 import SelectBox from '@commons/SelectBox';
+import supabase from '@/libs/api/supabase.api';
 import PROGRAMMING_LANGUAGES from '@/data/programmingLanguage.constant';
 
 export default function WritePage() {
-  const [programmingLanguage, setProgrammingLanguage] = useState(PROGRAMMING_LANGUAGES[0].name);
+  const [programmingLanguage, setProgrammingLanguage] = useState('');
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [errorMessage, setErrorMessage] = useState();
 
-  function handleWriteFormSubmit(e) {
+  const navigate = useNavigate();
+
+  async function handleWriteFormSubmit(e) {
     e.preventDefault();
-    // TODO: Supabase API 연결
+
+    if (title === '') return setErrorMessage('게시글의 제목을 작성해주세요.');
+    if (content === '') return setErrorMessage('게시글의 내용을 작성해주세요.');
+    if (programmingLanguage === '') return setErrorMessage('게시글의 내용의 언어 타입을 선택해주세요.');
+    setErrorMessage('');
+
+    const output = await supabase.from('posts').insert({ title, content, programming_language: programmingLanguage });
+    if (output.error) {
+      return alert('게시글을 생성하는데 실패했습니다.');
+    }
+    navigate('/');
   }
 
   return (
@@ -24,6 +39,7 @@ export default function WritePage() {
         <SelectBox
           size="sm"
           value={programmingLanguage}
+          placeholder="언어 타입을 선택해주세요."
           options={PROGRAMMING_LANGUAGES}
           onChange={setProgrammingLanguage}
         />
@@ -31,6 +47,7 @@ export default function WritePage() {
         <Input placeholder="제목" onChange={(e) => setTitle(e.target.value)} value={title} />
         <label>본문</label>
         <MDEditor value={content} onChange={setContent} height="400px" />
+        {errorMessage && <p>{errorMessage}</p>}
         <Button>작성 완료</Button>
       </form>
     </StContainer>
@@ -58,5 +75,10 @@ const StContainer = styled.main`
   form > button {
     float: right;
     margin-top: 10px;
+  }
+  form > p {
+    margin-top: 4px;
+    font-size: var(--font-size-sm);
+    color: var(--color-red);
   }
 `;
