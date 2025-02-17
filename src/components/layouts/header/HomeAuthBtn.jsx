822b8016ad20edb32ee6318f7a592790d3870a63
choichangyeon <changyon99@naver.com>
2025-02-17 10:06:05 +0900

    refactor:#46 sign up 페이지 연결

--- src/components/layouts/header/HomeAuthBtn.jsx
+++ src/components/layouts/header/HomeAuthBtn.jsx
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import ButtonLink from '@commons/ButtonLink';
-import { Link } from 'react-router-dom';
 
 export default function HeaderAuthBtn() {
   return (
