6516b2317fc3a0be56fe42a41db24549a845209d
dev_woo <mskim2525@naver.com>
2025-02-17 23:17:08 +0900

    remove, comment 추가 수정

--- src/components/features/view-page/CommentAreaContainer.jsx
+++ src/components/features/view-page/CommentAreaContainer.jsx
@@ -1,14 +1,28 @@
 import styled from 'styled-components';
 import CommentAddContainer from '@features/view-page/in-comment-area-container/CommentAddContainer';
 import CommentBoxContainer from '@features/view-page/in-comment-area-container/CommentBoxContainer';
+import { useEffect } from 'react';
+import supabase, { getComments } from '@/libs/api/supabase.api';
+import { useState } from 'react';
 
-export default function CommentAreaContainer({ comments, postId, nickname, authId }) {
+export default function CommentAreaContainer({ postId, nickname, authId, comments, onSubmit, onDelete }) {
   return (
     <StCommentAreaContainer>
       {comments.map((comment) => {
-        return <CommentBoxContainer key={comment.id} commentProps={comment} authId={authId} />; //본문에 달린 댓글에 대한 정보가 들어있는 props
+        return (
+          <CommentBoxContainer
+            key={comment.id}
+            onDelete={async () => {
+              const { error } = await supabase.from('comments').delete().eq('id', comment.id);
+              console.log(error);
+              onDelete(comment);
+            }}
+            commentProps={comment}
+            authId={authId}
+          />
+        ); //본문에 달린 댓글에 대한 정보가 들어있는 props
       })}
-      <CommentAddContainer postId={postId} nickname={nickname} />
+      <CommentAddContainer onSubmit={onSubmit} postId={postId} nickname={nickname} />
     </StCommentAreaContainer>
   );
 }
