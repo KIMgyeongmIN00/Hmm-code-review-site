8a3c1f0906f55fd582ce0bbac63a984224bab021
LSJ0706 <zosksldhs@gmail.com>
2025-02-17 22:34:17 +0900

    feat:#76 validation alert 추가

--- src/hooks/auth/useSignUpForm.js
+++ src/hooks/auth/useSignUpForm.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import supabase from '@/libs/api/supabase.api';
 import signUpValidate from '@/libs/utils/signUpValidate';
 import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 export default function useSignUpForm() {
   const [signUpFormData, setSignUpFormData] = useState({
@@ -24,6 +25,16 @@ export default function useSignUpForm() {
   const [isChecked, setIsChecked] = useState({ email: false, password: false });
   const navigate = useNavigate();
 
+  function isValidForm() {
+    for (const key in signUpFormData) {
+      const errorMsg = signUpValidate(key, signUpFormData[key], signUpFormData);
+      if (errorMsg && errorMsg !== '중복 체크를 해주세요.') {
+        return false;
+      }
+    }
+    return true;
+  }
+
   function signUpChangeHandler(e) {
     const { name, value } = e.target;
     setSignUpFormData((prev) => ({ ...prev, [name]: value }));
@@ -38,6 +49,12 @@ export default function useSignUpForm() {
 
   async function signUpCheckDuplicate(name) {
     if (name === 'email') {
+      const emailErrorMsg = signUpValidate('email', signUpFormData.email, signUpFormData);
+      if (emailErrorMsg !== '중복 체크를 해주세요.') {
+        setErrorMessage((prev) => ({ ...prev, email: emailErrorMsg }));
+        return;
+      }
+
       const { data } = await supabase.from('users').select().eq('email', signUpFormData.email);
       if (data.some((users) => users.email === signUpFormData.email)) {
         setErrorMessage((prev) => ({ ...prev, [name]: '사용 불가능한 이메일 입니다.' }));
@@ -46,6 +63,11 @@ export default function useSignUpForm() {
         setErrorMessage((prev) => ({ ...prev, [name]: '사용가능한 이메일 입니다.' }));
       }
     } else {
+      const nicknameErrorMsg = signUpValidate('nickname', signUpFormData.nickname, signUpFormData);
+      if (nicknameErrorMsg === '최소 2자부터 최대 8자까지 가능합니다.') {
+        setErrorMessage((prev) => ({ ...prev, nickname: nicknameErrorMsg }));
+        return;
+      }
       const { data } = await supabase.from('users').select().eq('nickname', signUpFormData.nickname);
       if (data.some((users) => users.nickname === signUpFormData.nickname)) {
         setErrorMessage((prev) => ({ ...prev, [name]: '사용 불가능한 닉네임 입니다.' }));
@@ -58,6 +80,17 @@ export default function useSignUpForm() {
 
   async function signUpSubmitHandler(e) {
     e.preventDefault();
+    // 유효성 검사 (오류가 있는지 확인)
+    if (!isValidForm()) {
+      Swal.fire({
+        title: 'Error!',
+        text: '입력하신 정보가 올바르지 않습니다. 다시 확인해주세요.',
+        icon: 'error',
+        confirmButtonText: '확인'
+      });
+      return;
+    }
+
     const { error } = await supabase.auth.signUp({
       email: signUpFormData.email,
       password: signUpFormData.password,
@@ -68,6 +101,21 @@ export default function useSignUpForm() {
           answer: signUpFormData.answer
         }
       }
+    });
+    if (error) {
+      Swal.fire({
+        title: 'Error!',
+        text: `회원가입 실패: ${error.message}`,
+        icon: 'error',
+        confirmButtonText: '확인'
+      });
+      return;
+    }
+    Swal.fire({
+      title: '성공!',
+      text: '회원가입 성공! 자동으로 로그인됩니다.',
+      icon: 'success',
+      confirmButtonText: '확인'
     });
     navigate('/sign-in');
   }
