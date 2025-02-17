5844e5f6679e0f0a91098476b8128a525034703a
SuimKim <github suim0215@gmail.com>
2025-02-17 21:49:14 +0900

    feat:#54 마이페이지 구현

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,21 +1,58 @@
 import Button from '@/components/commons/Button';
 import Input from '@/components/commons/Input';
+import PostCard from '@/components/commons/PostCard';
+import AuthContext from '@/contexts/auth/auth.context';
+import supabase from '@/libs/api/supabase.api';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useContext } from 'react';
 import { MdOutlinePerson } from 'react-icons/md';
 import styled from 'styled-components';
 
 function MyPage() {
+  const { auth } = useContext(AuthContext);
+
+  const [email, setEmail] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((_, session) => {
+      setEmail(session.user.email);
+    });
+    async function test() {
+      const testData = await supabase.from('posts').select('*').eq('user_id', auth.id);
+      setList(testData.data);
+    }
+    test();
+  }, []);
+
+  async function testFunc(e) {
+    e.preventDefault();
+
+    const { data, error } = await supabase.from('users').update({ nickname: nickname, email: email }).eq('id', auth.id);
+    console.log('error', error);
+  }
+
   return (
     <StMyPageContainer>
       <StUserInfoContainer>
         <StAvatar />
-        <StForm>
+        <StForm onSubmit={testFunc}>
           <StLabel htmlFor="email">이메일</StLabel>
-          <Input id="email" name="email" />
+          <Input id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
           <StLabel htmlFor="nickname">닉네임</StLabel>
-          <Input id="nickname" name="nickname" />
+          <Input id="nickname" name="nickname" value={nickname} onChange={(e) => setNickname(e.target.value)} />
+          <StButton type="submit">수정</StButton>
         </StForm>
-        <StButton>수정</StButton>
       </StUserInfoContainer>
+      <StPostContainer>
+        {list.map((post) => (
+          <StPostWrapper key={post.id}>
+            <PostCard postData={post} />
+          </StPostWrapper>
+        ))}
+      </StPostContainer>
     </StMyPageContainer>
   );
 }
@@ -63,4 +100,14 @@ const StButton = styled(Button)`
   right: 1rem;
 `;
 
+const StPostWrapper = styled.div`
+  width: 100%;
+  margin: 10px 0 10px 0;
+  padding-right: 20px;
+  box-sizing: border-box;
+`;
+
+const StPostContainer = styled.div`
+  margin-top: 20px;
+`;
 export default MyPage;
