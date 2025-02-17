7762363cdc4b2640a512a6f2fbb5f17644ab012b
llddang <bodyness@naver.com>
2025-02-17 22:02:03 +0900

    feat:#54 마이페이지 기능 완성

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -2,43 +2,57 @@ import Button from '@/components/commons/Button';
 import Input from '@/components/commons/Input';
 import PostCard from '@/components/commons/PostCard';
 import AuthContext from '@/contexts/auth/auth.context';
+import { onSignIn } from '@/contexts/auth/auth.reducer';
 import supabase from '@/libs/api/supabase.api';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { useContext } from 'react';
 import { MdOutlinePerson } from 'react-icons/md';
 import styled from 'styled-components';
+import Swal from 'sweetalert2';
 
 function MyPage() {
-  const { auth } = useContext(AuthContext);
+  const { auth, dispatch } = useContext(AuthContext);
 
   const [email, setEmail] = useState('');
   const [nickname, setNickname] = useState('');
   const [list, setList] = useState([]);
 
   useEffect(() => {
-    supabase.auth.onAuthStateChange((_, session) => {
-      setEmail(session.user.email);
-    });
-    async function test() {
-      const testData = await supabase.from('posts').select('*').eq('user_id', auth.id);
-      setList(testData.data);
+    setEmail(auth.email ?? '');
+    setNickname(auth.nickname ?? '');
+    async function getMyPosts() {
+      const { data } = await supabase.from('posts').select('*').eq('user_id', auth.id);
+      setList(data || []);
     }
-    test();
-  }, []);
+    getMyPosts();
+  }, [auth]);
 
-  async function testFunc(e) {
+  async function handleUpdateInfo(e) {
     e.preventDefault();
 
-    const { data, error } = await supabase.from('users').update({ nickname: nickname, email: email }).eq('id', auth.id);
-    console.log('error', error);
+    const { error } = await supabase.from('users').update({ nickname: nickname, email: email }).eq('id', auth.id);
+    if (error) {
+      return Swal.fire({
+        title: 'Error!',
+        text: '정보 수정에 실패했습니다.',
+        icon: 'error',
+        confirmButtonText: '확인'
+      });
+    }
+    dispatch(onSignIn(auth.id, email, nickname));
+    return Swal.fire({
+      title: 'Good job!',
+      text: '정보 수정에 성공했습니다.',
+      icon: 'success'
+    });
   }
 
   return (
     <StMyPageContainer>
       <StUserInfoContainer>
         <StAvatar />
-        <StForm onSubmit={testFunc}>
+        <StForm onSubmit={handleUpdateInfo}>
           <StLabel htmlFor="email">이메일</StLabel>
           <Input id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
           <StLabel htmlFor="nickname">닉네임</StLabel>
