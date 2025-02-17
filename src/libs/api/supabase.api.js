3eccc33ccce66025913901bc97fd6c5cc7e8c39a
dev_woo <mskim2525@naver.com>
2025-02-17 15:48:09 +0900

    chore: console.log 삭제

--- src/libs/api/supabase.api.js
+++ src/libs/api/supabase.api.js
@@ -1,10 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_HMM_SUPABASE_URL;
-console.log(`🚀 - supabase.api.js:4 - supabaseUrl:`, supabaseUrl);
 const supabaseKey = import.meta.env.VITE_HMM_SUPABASE_ANON_KEY;
-console.log(`🚀 - supabase.api.js:6 - supabaseKey:`, supabaseKey);
-
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
