37b305330dcc9a3754eb51ab202ef63e8a0331ce
choichangyeon <changyon99@naver.com>
2025-02-18 10:53:42 +0900

    refactor:오류 수정

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -43,7 +43,7 @@ export default function HomePage() {
     navigate('/');
     setLanguage(null);
     setSort('latest');
-  }, [renderHome]);
+  }, [navigate, renderHome]);
 
   return (
     <StHomePageContainer>
